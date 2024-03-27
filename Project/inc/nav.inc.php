9a36c39147f02461651359924d53fcfbe6192f50
Rifxt01 <145126679+Rifxt01@users.noreply.github.com>
2024-03-27 17:52:01 +0800

    Change to php

---
+++ Project/inc/nav.inc.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-md navbar-custom navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#" >
+                <img src="images/Logo.png" alt="Logo" style="height: 35px;">
+            </a>
+                        <!-- Toggler/collapsibe Button -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" aria-controls="collapsibleNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Navbar links -->
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                
+                <ul class="navbar-nav nav-custom ms-auto">
+    
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+    
+                    <li class="nav-item">
+                        <a class="nav-link" href="memberships.php">Memberships</a>
+                    </li>
+    
+                    <li class="nav-item">
+                        <a class="nav-link btn-custom" href="#">Sign In</a>
+                    </li>
+    
+                </ul>
+            </div>
+        </div>  
+    </nav>
---
+++ Project/memberships.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <link rel="stylesheet" href="css/membership.css">
+        <?php
+            include "inc/head.inc.php"
+        ?>
+    </head>
+<body>
+    <?php
+        include "inc/nav.inc.php";
+    ?>
+    <header class="S2">
+        <div class="heading">
+            <h1 id="headline">Our Membership Plans!</h1>
+        </div>
+    </header>
+    <main>
+        <section class="membership-plans">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <!-- Free Tier -->
+                    <div class="col-md-4">
+                        <div class="plan-box">
+                            <h3>Free Tier</h3>
+                            <ul>
+                                <li>Benefit 1</li>
+                                <li>Benefit 2</li>
+                                <li>More...</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary">Sign Up Now</a>
+                        </div>
+                    </div>
+                    <!-- Regular Tier -->
+                    <div class="col-md-4">
+                        <div class="plan-box">
+                            <h3>Regular Tier</h3>
+                            <ul>
+                                <li>Benefit 1</li>
+                                <li>Benefit 2</li>
+                                <li>More...</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary">Sign Up Now</a>
+                        </div>
+                    </div>
+                    <!-- Premium Tier -->
+                    <div class="col-md-4">
+                        <div class="plan-box">
+                            <h3>Premium Tier</h3>
+                            <ul>
+                                <li>Benefit 1</li>
+                                <li>Benefit 2</li>
+                                <li>More...</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary">Sign Up Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php
+    include "inc/footer.inc.php";
+    ?>
+</body>
+</html>
