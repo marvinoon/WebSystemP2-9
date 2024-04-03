a3d6e7e50d8dd2a1fad6ee3bac272050a6d6699c
Oon Jun Jie Marvin <2302045@sit.singaporetech.edu.sg>
2024-04-03 16:37:44 +0800

    added admin

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -45,6 +45,14 @@
                             <a class="nav-link btn-custom" href="logout.php">Logout</a>
                           </li>';
                         }
+                       else if (isset($_SESSION['admin_id'])) {
+                            echo '<li class="nav-item">
+                            <span class="nav-link btn-custom">Welcome, ' . $_SESSION['lname'] . '</span>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link btn-custom" href="logout.php">Logout</a>
+                          </li>';
+                        } 
                         else {
                             echo '<li class="nav-item">
                                     <a class="nav-link btn-custom" href="loginregister.php">Login</a>
