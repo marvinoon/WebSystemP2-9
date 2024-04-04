d888bce07f7362e28c9b2dd5305a77dd9246648e
Rifxt01 <145126679+Rifxt01@users.noreply.github.com>
2024-04-05 00:35:38 +0800

    accountpage css

--- account_page.php
+++ account_page.php
@@ -28,7 +28,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
-    <link rel="stylesheet" href="css/aboutus.css">
+    <link rel="stylesheet" href="css/accountpage.css">
     <link rel="stylesheet" href="css/nav.css">
     <link rel="stylesheet" href="css/footer.css">
 </head>
